ad548f200408a21d8489e55d2ba26c8bb15dbde7
francois nicaise <whitetigle@gmail.com>
2019-04-16 16:26:39 +0200

    Added new sample: call Fable from JS

---
+++ interopFableFromJS/src/App.fs
@@ -0,0 +1,7 @@
+module App
+
+open Fable.Core.JsInterop
+open Fable.Import
+open Fable.Core
+
+let sayHelloFable() = "Hello Fable!"
---
+++ interopFableFromJS/webpack.config.js
@@ -0,0 +1,26 @@
+// Note this only includes basic configuration for development mode.
+// For a more comprehensive configuration check:
+// https://github.com/fable-compiler/webpack-config-template
+
+var path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/App.fsproj",
+    output: {
+        path: path.join(__dirname, "./public"),
+        filename: "bundle.js",
+        libraryTarget: 'var',
+        library: 'MyFableLib'
+    },
+    devServer: {
+        contentBase: "./public",
+        port: 8080,
+    },
+    module: {
+        rules: [{
+            test: /\.fs(x|proj)?$/,
+            use: "fable-loader"
+        }]
+    }
+}
